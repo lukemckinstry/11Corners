df3a33716f4b5b3fa33d3ad6859aae66e174c1e3
lukemckinstry <lukemckinstry@gmail.com>
2017-10-16 16:41:23 -0400

    Update dijkstra recursion to return path

--- dijkstra-norecursion.py
+++ dijkstra-norecursion.py
@@ -7,7 +7,7 @@ import json
 from math import sqrt
 
 #network graph of geographic data
-ac_network_datafile = 'ac_network_crashes_distributed_FULL.json'
+delco_network_datafile = 'delco_crashes_in_geo_network.json'
 
 def string_to_float(input_string):
 	#*utility function bc json dict keys must be strings, in algo we need floats 
@@ -31,20 +31,23 @@ def find_nearest_node(input_point, network_data):
 			nearest_point = point
 	return nearest_point
 
-with open(ac_network_datafile) as data_file:    
+with open(delco_network_datafile) as data_file:    
     ac_network_data = json.load(data_file)
 
-#sample points in Allegheny County
-phipps =  tuple([-79.947843, 40.438784])
-pnc_park = tuple([-80.005655, 40.447083])
-near_pnc_park = tuple([-80.016136, 40.452080] )
-heinz_field = tuple([-80.017552, 40.447802])
-search_start_node_id = find_nearest_node( phipps , ac_network_data )
-search_end_node_id = find_nearest_node( heinz_field, ac_network_data )
+home = tuple([-75.345943, 39.903247])
+swat = tuple([-75.355627, 39.907071])
+media = tuple([-75.391199, 39.922603])
+
+search_start_node_id = find_nearest_node( home , ac_network_data )
+search_end_node_id = find_nearest_node( media, ac_network_data )
 
 visited = [] #init dijkstra visited list
 dist_so_far = {} #dijkstra queue
-dist_so_far[search_start_node_id] = [0, dist_between_nodes(search_start_node_id, search_end_node_id)]
+#fs -> distance from start node; tf -> distance from finish node; path -> nodes traversed on shortest path to current node
+dist_so_far[search_start_node_id] = { "fs": 0,
+					"tf": dist_between_nodes(search_start_node_id, search_end_node_id),
+					"path":[search_start_node_id] }
+
 #each node is two-item list... [tuple w/ lon/lat coords as floats, dict for each adjacent node w/ weight (i.e. dist, safety score) & coords]
 start_node = [search_start_node_id, ac_network_data.get( str(search_start_node_id) )]
 end_node = [search_end_node_id, ac_network_data.get( str(search_end_node_id) )]
@@ -57,19 +60,29 @@ def dijkstra( start_node, end_node ):
 		print("visited ", len(visited))
 		next_nodes = [ d for d in start_node[1] if "c_pt" not in d.keys() ]  #get adjacent nodes
 		if start_node == end_node:
-			print("FINISH")
 			found_it = True
-			return
+			return dist_so_far[start_node[0]]["path"] + [end_node[0]]
 		for next_node in next_nodes:
 			if tuple(next_node["end"]) not in [i[0] for i in dist_so_far]:	#add new node to queue	
-				dist_so_far[tuple(next_node["end"])] = [dist + next_node["dist"], dist_between_nodes(tuple(next_node["end"]), end_node[0]  )]
+				dist_so_far[tuple(next_node["end"])] = {"fs": dist + next_node["dist"],
+									"tf": dist_between_nodes(tuple(next_node["end"]), end_node[0]),
+									"path": dist_so_far.get(start_node[0])["path"] + [tuple(next_node["end"])] }
 			else:  #update shortest dist to node value in queue
 				if dist + next_node["dist"] < dist_so_far[tuple(next_node["end"])][0]:
-					dist_so_far[tuple(next_node["end"])] = [dist + next_node["dist"], dist_between_nodes( tuple(next_node["end"]), end_node[0] )]
+					dist_so_far[tuple(next_node["end"])] = {"fs": dist + next_node["dist"],
+										"tf": dist_between_nodes( tuple(next_node["end"]), end_node[0] ),
+										"path": dist_so_far.get(start_node[0])["path"] + [tuple(next_node["end"])] }
+
 		#return from queue possible next nodes to check 
-		available_unvisited = {k: [dist_so_far[k][0], dist_so_far[k][1]] for k in dist_so_far if k not in visited  }
+		available_unvisited = {k: [dist_so_far[k]["fs"], dist_so_far[k]["tf"]] for k in dist_so_far if k not in visited  }
 		nearest_to_dest = min(available_unvisited, key=lambda x: available_unvisited[x][1] ) #first check node in queue that is closest to destination
 		start_node = [nearest_to_dest, ac_network_data.get(str(nearest_to_dest))]
-		dist = dist_so_far[nearest_to_dest][0]
+		dist = dist_so_far[nearest_to_dest]["fs"]
 
-dijkstra(start_node, end_node )
+nav_path = dijkstra(start_node, end_node )
+
+plot_path = [[i[0],i[1]] for i in nav_path  ]
+
+print("nav_path -->", plot_path)
+print( "number of nodes visited",  len(plot_path) )
+
