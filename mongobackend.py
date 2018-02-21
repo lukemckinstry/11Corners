8c732de524bc2948bf192a42da1966668c39b330
Luke <lukemckinstry@gmail.com>
2018-02-21 14:49:23 -0500

    standardize slice ranges

--- mongobackend.py
+++ mongobackend.py
@@ -3,6 +3,7 @@
 import json
 import pymongo
 from pymongo import MongoClient
+import math
 
 client = MongoClient('localhost', 27017)
 db = client['delco-network']
@@ -23,27 +24,29 @@ def get_data(geojson_datafile):
 	return output_dict
 
 
-def prep_slice_dict(feature_dict, slice_network, NUM_SLICES):
-	slice_max = max([key[0] for key in feature_dict])
-	slice_min = min([key[0] for key in feature_dict])
-	incrementer = (slice_max - slice_min) / NUM_SLICES
-	for i in range(0, NUM_SLICES):
-		slice_network[tuple([slice_max - ( incrementer * i), slice_max - ( incrementer * (i+1)) ])] = {}
-	return
-
-def make_slices(feature_dict, slice_network):
-	slice_keys = [k for k in slice_network]
-	print( "slice keys",  slice_keys )
-	for feature in feature_dict:
-		insert_key = [k for k in slice_keys if feature[0] <= k[0] and feature[0] >= k[1] ][0]
-		slice_network[insert_key][str(feature).replace('.','&#46;')] = feature_dict[feature]
-		#str(feature).replace('.','&#46;')
+def prep_slice_dict(feature_dict, slice_network):
+	slices = set([ math.floor(f[0] / 0.05) * 0.05 for f in feature_dict  ])
+	for s in slices:
+		subdict = { str(x).replace('.','&#46;'): feature_dict[x] for x in feature_dict if x[0] >= s and x[0] < s + 0.05}
+		slice_network[ tuple([s,s+0.05])] = subdict	
 	return
 
 def insert_to_mongo( slice_network ):
 	for slice_key in slice_network:
 		print(  "HAPPENING ", slice_key)
-		db.slices.save( { str(slice_key).replace('.','&#46;') : slice_network[slice_key] } )
+		db.slices.save( { 'latmin': slice_key[0], 'latmax':slice_key[1], 'nodes' : slice_network[slice_key] } )
+	return
+
+def querymongo():
+	slices = db.slices
+	cursor = slices.find( {})
+	for document in cursor:
+          print( document['latmin'], document['latmax'])
+          print( len(document['nodes']) )
+	#print(  "GOT A SLICE", len( slice_obj  ))
+	#print( slice_obj )
+	print( "OUR WORK IS FINISHED" )
+	return
 
 
 def sanity_check(network_graph):
@@ -75,15 +78,14 @@ def write_to_file(compress_graph, outfile):
 	return
 
 def main(infile, outfile):
-	NUM_SLICES = 10
 	features = get_data(infile)
-	total = len(features)
-	print( total )
+	
 
 	slice_network = {}
-	prep_slice_dict( features, slice_network, NUM_SLICES )
-	make_slices( features, slice_network )
-	#insert_to_mongo( slice_network )
+	prep_slice_dict( features, slice_network )
+	insert_to_mongo( slice_network )
+
+	querymongo()
 	
 	return
 
