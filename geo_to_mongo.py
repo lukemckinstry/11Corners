e49d6a9ddf9f51a0c74763d17fa815f8920c3a70
Luke <lukemckinstry@gmail.com>
2018-03-01 13:34:02 -0500

    geo to mongo fix tile keys to elim duplicate docs in mongo collection

--- geo_to_mongo.py
+++ geo_to_mongo.py
@@ -14,9 +14,6 @@ if len(sys.argv) != 2:
 
 infile = sys.argv[1]
 
-
-
-
 ### Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
 #uri = 'mongodb://user:pass@host:port/db'
 client = pymongo.MongoClient(mongo_uri)
@@ -27,10 +24,18 @@ db = client.get_default_database()
 #db = client['delco-network']
 
 def string_to_float(input_string):
+	'''
+	for reading data from file i.e not mongo db
+	converts key in mongo dict to float
+	'''
 	item = input_string.split(",")
 	return tuple([ float(point.replace("(","").replace(")","")) for point in item])
 
 def get_data(geojson_datafile):
+	'''
+	for reading data from file i.e not mongo db
+	reads json object network graph from file to python dict 
+	'''
 	with open(geojson_datafile) as data_file:
 		data = json.load(data_file)
 	output_dict = {}
@@ -41,9 +46,16 @@ def get_data(geojson_datafile):
 
 
 def round_nearest(x, a):
+	'''
+	floors floating point to 0.05, with precision 
+	'''
     return round(round(x / a) * a, -int(math.floor(math.log10(a))))
 
 def prep_insert_dict(feature_dict, prep_network):
+	'''
+	for reading data from file i.e not mongo db
+	adds 4-part keys (minlon, maxlon, minlat. maxlat) to python dict to store each network graph point 
+	'''
 	slices = sorted(set([ round_nearest(f[0], 0.05) for f in feature_dict  ]))
 	for s in slices:
 		subdict = { str(x).replace('.','&#46;'): feature_dict[x] for x in feature_dict if x[0] >= s and x[0] < s + 0.05}
@@ -52,63 +64,82 @@ def prep_insert_dict(feature_dict, prep_network):
 
 
 def prep_slice_dict(feature_dict, slice_network):
-	lon_slices = sorted(set([ round_nearest(f[0], 0.05) for f in feature_dict  ]))
-	lat_slices = sorted(set([ round_nearest(f[1], 0.05) for f in feature_dict  ]))
+	'''
+	for reading data from mongo db, not file
+	adds subdict of network graph points to new python dict id'd  by 4-part keys (minlon, maxlon, minlat. maxlat) 
+	'''
+	
+	lon_breaks = sorted(set([ round_nearest(f[0], 0.05) for f in feature_dict  ]))
+	lat_breaks = sorted(set([ round_nearest(f[1], 0.05) for f in feature_dict  ]))
+	lon_slices = [tuple([s, round_nearest(s+0.05, 0.05)]) for s in lon_breaks ] 
+	lat_slices = [tuple([s, round_nearest(s+0.05, 0.05)]) for s in lat_breaks ] 
+
 	for lat_s in lat_slices:
 		for lon_s in lon_slices:
 			subdict = { str(x).replace('.','&#46;'): feature_dict[x] for x in feature_dict if 
-							x[0] >= lon_s and x[0] < lon_s + 0.05  and
-							x[1] >= lat_s and x[1] < lat_s + 0.05	}
-			lon_id = tuple([lon_s,round_nearest(lon_s+0.05,0.05) ])
-			lat_id = tuple([lat_s,round_nearest(lat_s+0.05,0.05) ])
-			slice_network[ tuple([lon_id, lat_id]  )  ] = subdict	
+							x[0] >= lon_s[0] and x[0] < lon_s[1] and
+							x[1] >= lat_s[0] and x[1] < lat_s[1] }
+			lon_id = tuple([lon_s[0],lon_s[1] ])
+			lat_id = tuple([lat_s[0],lat_s[1] ])
+			#print( "PREP -> ", lon_id, lat_id, len(subdict) )
+			if len(subdict) > 0:
+				slice_network[ tuple([lon_id, lat_id] )  ] = subdict	
 	return
 
 def query_tile(tile_key):
-	print( "QUERY-TILE", tile_key, tile_key[0][0],tile_key[0][1], tile_key[1][0], tile_key[1][1] )
+	'''
+	get a doc (json obj) from geo slices collection of mongo db, using 4-part key that ids it 
+	return none if doc does not exist i.e. not yet data for that tile in db
+	'''
 	cursor = db.slices.find_one( {	'lonmin' : { '$eq' : tile_key[0][0]  } ,
 									'lonmax' : { '$eq' : tile_key[0][1]  } ,
 									'latmin' : { '$eq' : tile_key[1][0]  } ,
 									'latmax' : { '$eq' : tile_key[1][1]  } ,
 									  })	
-	# for document in cursor:
-	# 	print( len(document['nodes']) )
 	if cursor:
-		print("KEYS IN CURSOR -->", cursor.keys())
 		cursor = cursor['nodes']
 	return cursor
 
 def insert_to_mongo( slice_network ):
+	'''
+	insert data to mongo db, if tile exists update it, otherwise create new doc for tile in db
+	'''
 	for slice_key in slice_network:
 		existing_dict = query_tile( slice_key )
 		if existing_dict:
-			print( "DOC ALREADY EXISTS, UPDATING OLD ONE", slice_key  )
-			#print( "BEFORE UPDATE ", len(existing_dict ) )
+			#DOC ALREADY EXISTS, UPDATING OLD ONE
 			existing_dict.update( slice_network[slice_key] )
-			#print( "AFTER UPDATE ", len(existing_dict ) )
 			db.slices.update( { 'lonmin': slice_key[0][0], 'lonmax':slice_key[0][1],
 								'latmin': slice_key[1][0], 'latmax':slice_key[1][1], },
 							{ 	'lonmin': slice_key[0][0], 'lonmax':slice_key[0][1],
 								'latmin': slice_key[1][0], 'latmax':slice_key[1][1],
 								'nodes' : existing_dict } )
 		else:
-			print( "NO EXISTING DOC, SAVING NEW ONE", slice_key  )
+			#NO EXISTING DOC, SAVING NEW ONE
 			existing_dict = slice_network[slice_key]
 			db.slices.save( {	'lonmin': slice_key[0][0], 'lonmax':slice_key[0][1],
 								'latmin': slice_key[1][0], 'latmax':slice_key[1][1],
 								'nodes' : existing_dict } )		
 	return
 
-
 def querymongo():
-	#slices = db.slices
-	pointlat = -75.28
-	cursor = db.slices.find( { 'lonmin' : { '$lt' : pointlat  } , 'lonmax' : { '$gt' : pointlat  }   })	
+	'''
+	sanity check, query a doc (tile) from db, or all docs in collection
+	currently set up to solve duplicate docs issue
+	'''
+	#cursor = db.slices.find( { 'lonmin' : { '$lt' : pointlat  } , 'lonmax' : { '$gt' : pointlat  }   })	
+	cursor = db.slices.find( { })	
 	total = 0
+	doc_list = []
 	for document in cursor:
-		print( len(document['nodes']) )
-		total += len(document['nodes'])
-	print( "TOTAL --> ", total )
+		doc_id = tuple([ document['lonmin'], document['lonmax'], document['latmin'], document['latmax']]) 
+		doc_list.append( doc_id )
+	#print( "TOTAL DOCS--> ", len(doc_list), "SET --> ", len( set(doc_list) ) )
+	multiples = []
+	for d in set(doc_list):
+		if doc_list.count(d) > 1:
+			multiples.append(d)
+	#print( "##### -> ", [ [f, doc_list.count(f)] for f in multiples] )
 	return
 
 def main(infile):
